a3eacd29ae8ff541f7eee18f3b4dfa78fa8ab4d7
Wasupol Seetalarom <pop221540@gmail.com>
2018-05-26 20:24:12 +0700

    Add post

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const app = express()
-let contacts = [{name:'Test',phoneNumber:'0899775445'}]
+let contacts = [{name:'Test',phoneNumber:'0899775445'},{name:'Test2',phoneNumber:'0819778545'}]
 
 app.use(bodyParser.json())
 app.use(cors())
@@ -13,9 +13,12 @@ app.get('/contacts', (req, res) =>{
     res.json(contacts)
 })
 
-
 /// TODO: Develop POST /contacts
-
+app.post('/contacts', (req,res) => {
+    let newContact = req.body
+    contacts.push(newContact)
+    res.json(newContact)
+})
 
 
 app.listen(3000, () => {
